02645d16d4f2da67dc4bbb424061d96076ae11ec
Thierry <titimoby@gmail.com>
2022-01-25 11:49:24 +0100

    Clue TFT example is working

--- Clue/02-TFT-Test/main.go
+++ Clue/02-TFT-Test/main.go
@@ -8,18 +8,31 @@ import (
 	"tinygo.org/x/drivers/st7789"
 )
 
-var red = color.RGBA{255, 0, 0, 255}
-var green = color.RGBA{0, 255, 0, 255}
+var (
+	display st7789.Device
+	red     = color.RGBA{255, 0, 0, 255}
+	green   = color.RGBA{0, 255, 0, 255}
+)
 
 func main() {
-	machine.SPI0.Configure(machine.SPIConfig{
-		Frequency: 16000000,
-		SCK:       machine.SPI0_SCK_PIN,
-		SDO:       machine.SPI0_SDO_PIN,
-		SDI:       machine.SPI0_SDI_PIN,
+	machine.SPI1.Configure(machine.SPIConfig{
+		Frequency: 8000000,
+		SCK:       machine.TFT_SCK,
+		SDO:       machine.TFT_SDO,
+		SDI:       machine.TFT_SDO,
+		Mode:      0,
 	})
-	display := st7789.New(machine.SPI0, machine.TFT_RESET, machine.TFT_DC, machine.TFT_CS, machine.TFT_LITE)
-	display.Configure(st7789.Config{})
+	display = st7789.New(machine.SPI1,
+		machine.TFT_RESET,
+		machine.TFT_DC,
+		machine.TFT_CS,
+		machine.TFT_LITE)
+	display.Configure(st7789.Config{
+		Rotation:   st7789.ROTATION_180,
+		Height:     320,
+		FrameRate:  st7789.FRAMERATE_111,
+		VSyncLines: st7789.MAX_VSYNC_SCANLINES,
+	})
 
 	i := 0
 	for {
